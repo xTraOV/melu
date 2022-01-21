9bee56817bbb513e3c8fe4233f6526bdc4bd8fe1
xTraOV <vazhakobalava@gmail.com>
2022-01-21 11:27:13 +0400

    homepage

--- src/components/get-products/get-products.jsx
+++ src/components/get-products/get-products.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const GetProducts = async () => {
-    const response = await fetch(`https://fakestoreapi.com/products`);
+    const response = await fetch(`https://fakestoreapi.com/products?limit=5`);
     const data = await response.json();
     return data;
 };
--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -2,19 +2,35 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react/cjs/react.development";
 import GetProducts from "../components/get-products/get-products";
+import "./home.page.style.scss";
 
 const HomePage = () => {
     const [myData, setMyData] = useState(null);
     useEffect(() => {
-        GetProducts().then(
-            (p) =>
-                categorySet.add(p.map((p) => p.category)) &
-                console.log(p.map((p) => p.category) & setMyData(p))
-        );
+        GetProducts().then((p) => setMyData(p));
     }, []);
 
-    const categorySet = new Set();
-    return <div></div>;
+    return (
+        <div className='home-grid'>
+            {myData &&
+                myData.map((p) => (
+                    <div
+                        className='home-product-wrapper'
+                        id={`id${p.id}`}
+                        key={p.id}>
+                        <div className='home-product-header'>
+                            <h1>{p.title}</h1>
+                        </div>
+                        <img
+                            className='home-product-image'
+                            width='100%'
+                            src={p.image}
+                            alt={p.title}
+                        />
+                    </div>
+                ))}
+        </div>
+    );
 };
 
 export default HomePage;
