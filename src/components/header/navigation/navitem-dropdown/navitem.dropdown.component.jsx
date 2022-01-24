0684d130f341143905b0949086373fcfe743358b
xTraOV <vazhakobalava@gmail.com>
2022-01-24 13:44:01 +0400

    ad

---
+++ src/components/header/navigation/navitem-dropdown/navitem.dropdown.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavDropDown = ({ props }) => (
+    <div className={props}>
+        <ul>
+            <li>საფულე</li>
+            <li>ჩანთა</li>
+            <li>ქამარი</li>
+            <li>აქსესუარი</li>
+        </ul>
+    </div>
+);
+
+export default NavDropDown;
